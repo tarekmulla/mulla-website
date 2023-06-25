491b94a8f0c7b05226def97238ff849922f912af
Tarek Mulla <tarek@mulla.au>
2023-06-25 15:27:28 +1000

    delete project tag

--- webapp/src/components/Projects/allProjects.jsx
+++ webapp/src/components/Projects/allProjects.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 
-import Project from "./project";
 
-import INFO from "../../data/user";
 
 import "./styles/allprojects.css";
 
 const Allprojects = () => {
   return (
     <div>
-      <div className="all-projects-container">
-        {INFO.projects.map((project, index) => (
-          <div className="all-projects-project" key={index}>
-            <Project
-              logo={project.logo}
-              title={project.title}
-              description={project.description}
-              linkText={project.linkText}
-              link={project.link}
-            />
-          </div>
-        ))}
-      </div>
+
     </div>
   );
 };
